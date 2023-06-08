a853c359e94a2850ece66079b9635cbb8851af98
Chowon-Hasan <chowonhasan7@gmail.com>
2023-06-09 03:52:10 +0600

    added admin api and instructor api

--- index.js
+++ index.js
@@ -27,12 +27,17 @@ async function run() {
     const classes = client.db("meloMusicDb").collection("musicClasses");
     const instructor = client.db("meloMusicDb").collection("instructor");
     const addClasses = client.db("meloMusicDb").collection("addClasses");
+    const studentsCollection = client
+      .db("meloMusicDb")
+      .collection("allStudents");
 
+    // GETITING ALL THE CLASSES DATA FROM DB
     app.get("/classes", async (req, res) => {
       const result = await classes.find().sort({ students: -1 }).toArray();
       res.send(result);
     });
 
+    // GET ALL INSTRUCTOR DATA FROM DB
     app.get("/instructor", async (req, res) => {
       const result = await instructor
         .find()
@@ -41,18 +46,67 @@ async function run() {
       res.send(result);
     });
 
+    // CREATE STUDENTS AS A USER COLLECTION
+    app.post("/students", async (req, res) => {
+      const student = req.body;
+      const query = { email: student.email };
+      const existinStudent = await studentsCollection.findOne(query);
+      console.log(existinStudent);
+      if (existinStudent) {
+        return res.send({ message: "user already exits" });
+      }
+      const result = await studentsCollection.insertOne(student);
+      res.send(result);
+    });
+
+    // GET ALL STUDENTS FROM DB
+    app.get("/allstudents", async (req, res) => {
+      const result = await studentsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // MAKE ADMIN API
+    app.patch("/allstudents/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await studentsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // MAKE instructor API
+    app.patch("/allstudents/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await studentsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // GET THE SPECIFIC CLASS DATA FROM DB
     app.get("/addclasses/:id", async (req, res) => {
       const id = req.params.id;
       const result = await addClasses.findOne({ classID: id });
       res.send(result);
     });
 
+    // GET MYCLASSES FROM DB
     app.get("/myclasses/:email", async (req, res) => {
       const result = await addClasses
         .find({ email: req.params.email })
         .toArray();
       res.send(result);
     });
+
+    // DELETED CLASS FROM DB
     app.delete("/myclasses/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
@@ -63,6 +117,7 @@ async function run() {
       res.send(result);
     });
 
+    // POST DATA IN DB ADD CLASSES FOR A STUDENT
     app.post("/addclasses", async (req, res) => {
       const addClassData = req.body;
       console.log(addClassData);
